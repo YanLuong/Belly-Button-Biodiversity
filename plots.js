3b75f975d42b5c341925007abb047f9df1527f24
Yan Kin Luong <yan.kin.luong@me.com>
2021-05-24 12:33:32 -0500

    12.4.3

--- plots.js
+++ plots.js
@@ -1,49 +1,42 @@
+// 12.4.3: Belly Button Demographics Panel
 
-// var trace = {
-//     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-//     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//     type: "bar"
-//    };
-//    var data = [trace];
-//    var layout = {
-//     title: "'Bar' Chart",
-//     xaxis: { title: "Drinks"},
-//     yaxis: { title: "% of Drinks Ordered"}
-//    };
-//    Plotly.newPlot("plotArea", data, layout);
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-var trace = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-   };
-   var data = [trace];
-   var layout = {
-    title: "'Pie' Chart",
-   };
-   Plotly.newPlot("plotArea", data, layout);
+    PANEL.html("");
 
-
-// var trace = {
-//     x: ["burrito", "pizza", "chicken"],
-//     y: [10, 18, 5],
-//     type: "bar"
-// };
-
-// var layout = {
-//     title: "Luncheon Survey",
-//     xaxis: {title: "Food Option"},
-//     yaxis: {title: "Number of Respondents"}
-// };
-
-// Plotly.newPlot("plotArea", [trace], layout);
+    Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+}
 
 
+function optionChanged(newSample) {
+    console.log(newSample);
+    buildMetadata(newSample);
+    // buildCharts(newSample);
+  }
+  
+init();
 
-//Plotly.newPlot("plotArea", [{x:[5,10,15],y:[10,20,30]}]);
 
-//Plotly.newPlot(“plotArea”, [{x:[5,10,15],y:[10,20,30]}]);
-
-//[{x: [1, 2, 3], y: [10, 20, 30]}]);
+ 
